7facb032beb96e4e69cd5bb4bb402a304dbb22ff
Sandeep Shukla <sandeepini.2012@gmail.com>
2021-02-13 16:37:00 +0530

    second commit

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,14 @@
 const express = require("express");
-
 const router = express.Router();
+var Post = require('../models/Post');  //error comming when tyring to add 
 
 // this route will come only localhost:3000/posts , since we are using middleware and triggering from there
 router.get("/", (req, res) => {
   res.send("Hello from post");
+});
+
+router.post('/',(req,res)=>{
+  console.log(req.body);
 });
 
 module.exports = router;
