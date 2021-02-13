9afc24249c5900f1173a4375e5540c3b07d246c8
Sandeep Shukla <sandeepini.2012@gmail.com>
2021-02-13 17:46:03 +0530

    patch method completed

--- routes/posts.js
+++ routes/posts.js
@@ -51,4 +51,18 @@ router.delete('/:postId',async(req,res)=>{
   }
 });
 
+//Update a post 
+router.patch('/:postId' , async (req,res)=>{
+
+  try{
+    const updatedPost = await Post.updateOne({_id: req.params.postId},{$set : {title: req.body.title}});
+    res.json(updatedPost);
+
+
+  }catch(err){
+    res.json({message : err});
+  }
+
+});
+
 module.exports = router;
