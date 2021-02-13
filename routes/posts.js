39949fcc2dd678d20409b26e74e2874de434f16a
Sandeep Shukla <sandeepini.2012@gmail.com>
2021-02-13 17:27:45 +0530

    POST complete 3.0 data showing /posts/:postID specificPost  route

--- routes/posts.js
+++ routes/posts.js
@@ -3,16 +3,18 @@ const router = express.Router();
 var Post = require("../models/Post");
 
 // this route will come only localhost:3000/posts , since we are using middleware and triggering from there
-router.get("/", async (req, res) => {
-   try{
-     const posts = await Post.find();
-     res.json(posts);
 
-   }catch(err){
-     res.json({message : err});
-   }
+//GET BACK ALL THE POST
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
+//SUBMIT A POST
 router.post("/", async (req, res) => {
   const post = new Post({
     title: req.body.title,
@@ -24,6 +26,18 @@ router.post("/", async (req, res) => {
     res.json(savePost);
   } catch {
     res.json({ message: err });
+  }
+});
+
+//SPECIFIC POST
+router.get("/:postId", async (req, res) => {
+  // console.log(req.params.postId);
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
   }
 });
 
