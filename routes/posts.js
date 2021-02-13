8c0b55b20a2d798a131980d61eb14689566b2f31
Sandeep Shukla <sandeepini.2012@gmail.com>
2021-02-13 17:11:30 +0530

    POST complete 1.0

--- routes/posts.js
+++ routes/posts.js
@@ -1,26 +1,24 @@
 const express = require("express");
 const router = express.Router();
-var Post = require('../models/Post');
-
+var Post = require("../models/Post");
 
 // this route will come only localhost:3000/posts , since we are using middleware and triggering from there
 router.get("/", (req, res) => {
   res.send("Hello from post");
 });
 
-router.post('/',(req,res)=>{
+router.post("/", async (req, res) => {
   const post = new Post({
     title: req.body.title,
-    description: req.body.description
+    description: req.body.description,
   });
 
-  post.save()
-  .then(data => {
-    res.json(data);
-  })
-  .catch(err=>{
-    res.json({message: err});
-  });
+  try {
+    const savePost = await post.save();
+    res.json(savePost);
+  } catch {
+    res.json({ message: err });
+  }
 });
 
 module.exports = router;
